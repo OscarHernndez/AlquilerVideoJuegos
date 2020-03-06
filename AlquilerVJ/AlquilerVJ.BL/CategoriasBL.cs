5e9c692d5edc4ae562650fb8ebfade1a85c282a5
oscarhernandez <oscar1001996@live.com>
2020-03-06 00:16:52 -0600

    Crear validaciones de sus mantenimientos y agregar imagenes a los mantenimientos donde aplique

---
+++ AlquilerVJ/AlquilerVJ.BL/CategoriasBL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVJ.BL
+{
+    public class CategoriasBL
+    {
+        Contexto _contexto;
+        public List<Categoria> ListadeCategoria{ get; set; }
+
+        public CategoriasBL()
+        {
+            _contexto = new Contexto();
+            ListadeCategoria = new List<Categoria>();
+        }
+
+        public List<Categoria> ObtenerCategorias()
+        {
+
+            ListadeCategoria = _contexto.Categorias.ToList();
+     
+
+            return ListadeCategoria; 
+        }
+
+        public void GuardarCategoria(Categoria categoria)
+        {
+            if (categoria.Id == 0)
+            {
+                _contexto.Categorias.Add(categoria);
+            }
+            else
+            {
+                var productoExistent = _contexto.Categorias.Find(categoria.Id);
+                productoExistent.Descripcion = categoria.Descripcion;
+            }
+            _contexto.SaveChanges();
+
+        }
+
+        public void EliminarCategoria(int id)
+        {
+            var categoria = _contexto.Categorias.Find(id);
+            _contexto.Categorias.Remove(categoria);
+            _contexto.SaveChanges();
+
+        }
+
+        public Categoria ObtenerCategorias(int id)
+        {
+            var categoria = _contexto.Categorias.Find(id);
+            return categoria;
+        }
+    }
+}
---
+++ AlquilerVJ/AlquilerVJ.WebAdmin/Controllers/ProductosController.cs
@@ -0,0 +1,125 @@
+﻿using AlquilerVJ.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlquilerVJ.WebAdmin.Controllers
+{
+    public class ProductosController : Controller
+    {
+        ProductosBL _ProductosBL;
+        CategoriasBL _CategoriasBL;
+
+        public ProductosController()
+        {
+            _ProductosBL = new ProductosBL();
+            _CategoriasBL = new CategoriasBL();
+        }
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var ListadeProductos = _ProductosBL.ObtenerProductos();
+
+            return View(ListadeProductos);
+        }
+
+        public ActionResult Crear()
+        {
+            var NuevoProducto = new Producto();
+            var categorias = _CategoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
+
+            return View(NuevoProducto);
+            
+        }
+
+        private string GuardarImagen(HttpPostedFileBase imagen)
+        {
+            string path = Server.MapPath("~/Imagenes/" + imagen.FileName);
+            imagen.SaveAs(path);
+
+            return "/Imagenes/" + imagen.FileName;
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Producto producto, HttpPostedFileBase imagen)
+        {
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
+                _ProductosBL.GuardarProducto(producto);
+                return RedirectToAction("Index");
+            }
+                var categorias = _CategoriasBL.ObtenerCategorias();
+                ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
+
+                return View(producto);
+        }
+
+
+        public ActionResult Editar(int id)
+        {
+            var producto = _ProductosBL.ObtenerProducto(id);
+            var categorias = _CategoriasBL.ObtenerCategorias();
+
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion", producto.CategoriaId);
+
+            return View(producto);
+        }
+
+
+        [HttpPost]
+        public ActionResult Editar(Producto producto, HttpPostedFileBase imagen)
+        {
+            if (ModelState.IsValid)
+            {
+                if (producto.CategoriaId == 0)
+                {
+                    ModelState.AddModelError("CategoriaId", "Seleccione una categoria");
+                    return View(producto);
+                }
+
+                _ProductosBL.GuardarProducto(producto);
+                return RedirectToAction("Index");
+            }
+            var categorias = _CategoriasBL.ObtenerCategorias();
+            ViewBag.CategoriaId = new SelectList(categorias, "Id", "Descripcion");
+
+            if (imagen != null)
+            {
+                producto.UrlImagen = GuardarImagen(imagen);
+            }
+
+            return View(producto);
+        }
+
+        public ActionResult Detalles(int id)
+        {
+            var producto = _ProductosBL.ObtenerProducto(id);
+            return View(producto);
+        }
+
+
+        public ActionResult Eliminar(int id)
+        {
+            var producto = _ProductosBL.ObtenerProducto(id);
+
+            return View(producto);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(Producto producto)
+        {
+            _ProductosBL.EliminarProducto(producto.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
